28edc32ee78dda5643ebd1cec15af8bbbcd9f347
Robert Hafner <tedivm@tedivm.com>
2017-10-16 21:26:24 -0700

    Fill in package.json, add `styletest`, remove semicolons

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>I love consensus!</h1>');
-});
+  res.send('<h1>I love consensus!</h1>')
+})
 
 app.get('/hello', (req, res) => {
-  res.send('<h1>hello!</h1>');
-});
+  res.send('<h1>hello!</h1>')
+})
 
 app.listen(3000, () => {
   console.log('The application is running on localhost:3000')
-});
+})
